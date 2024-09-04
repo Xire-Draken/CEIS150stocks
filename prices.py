12fb48c81db582761df90a78377316a1de34426a
Wayne <WayneE.Weeks@gmail.com>
2024-09-04 16:44:55 -0400

    rough draft code

--- prices.py
+++ prices.py
@@ -1,1 +1,30 @@
-import time
+
+#Creation of a stock pricing program
+#initialize the count variable to 0
+count = 0
+#initialize the sum variable to 0
+total_sum = 0
+#input full_name
+full_name = str(input("What is your name: "))
+#input the min_price and convert it to float
+min_price = float(input("What is the minimum price "))
+#create a list of prices example: price_list = [69.0, 71.0, 84.5, 91.0, 67.4, 81.2, 84.6, 58.8,
+#79.3, 101.2]
+price_list = [69.0, 71.0, 84.5, 91.0, 67.4, 81.2, 84.6, 58.8,
+79.3, 101.2]
+
+#for price in price_list
+for price in price_list:
+#add current price to sum
+    total_sum += price
+#if price > min_price
+#increment count by 1
+    if price > min_price:
+        count += 1
+
+#output "Hello",name,"the minimum price is ",min_price
+#output "There are ",count,"prices greater than the minimum price"
+#output "The total price is ",sum
+print("Hello",full_name, "the minimum price is ",min_price)
+print("There are",count,"prices greater than the minimum price")
+print("The total price is ", total_sum)
