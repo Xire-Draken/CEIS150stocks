4dfc5feae96330369df061fffda6e5bddd6a7b15
Wayne <WayneE.Weeks@gmail.com>
2024-09-04 17:14:22 -0400

    additional output option

--- prices.py
+++ prices.py
@@ -4,10 +4,10 @@
 count = 0
 #initialize the sum variable to 0
 total_sum = 0
-#input full_name
-full_name = str(input("What is your name: "))
+#input full_name (strip removes whitespace from front and back of input)
+full_name = str(input("What is your name? ").strip())
 #input the min_price and convert it to float
-min_price = float(input("What is the minimum price "))
+min_price = float(input("What is the your minimum price? "))
 #create a list of prices example: price_list = [69.0, 71.0, 84.5, 91.0, 67.4, 81.2, 84.6, 58.8,
 #79.3, 101.2]
 price_list = [69.10, 71.50, 84.45, 91.50, 67.84, 81.22, 84.64, 58.88,
@@ -25,6 +25,13 @@ for price in price_list:
 #output "Hello",name,"the minimum price is ",min_price
 #output "There are ",count,"prices greater than the minimum price"
 #output "The total price is ",sum
-print("Hello",full_name, "the minimum price is ",min_price)
+print("Hello",full_name,"the minimum price is",min_price)
 print("There are",count,"prices greater than the minimum price")
-print("The total price is ", total_sum)
+print("The total price is ",total_sum)
+
+#the ouput could also be written by this example too.
+#This way is better looking and easier to read in my opinion
+#also 
+print(f"Hello {full_name}, the minimum price is {min_price:.2f}.")
+print(f"There are {count} prices greater than the minimum price.")
+print(f"The total price is {total_sum:.2f}.")
